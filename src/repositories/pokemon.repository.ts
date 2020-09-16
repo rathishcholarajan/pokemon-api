d9da4ae2fca6d8fb555a4e532d59484b1387a04a
Rathish Cholarajan <rathishc24@gmail.com>
2020-09-15 21:37:23 -0400

    feat(repository): create pokemon repository

---
+++ src/repositories/pokemon.repository.ts
@@ -0,0 +1,14 @@
+import {inject} from '@loopback/core';
+import {DefaultCrudRepository} from '@loopback/repository';
+import {MongoDataSource} from '../datasources';
+import {Pokemon, PokemonRelations} from '../models';
+
+export class PokemonRepository extends DefaultCrudRepository<
+  Pokemon,
+  typeof Pokemon.prototype.id,
+  PokemonRelations
+  > {
+  constructor(@inject('datasources.mongo') dataSource: MongoDataSource) {
+    super(Pokemon, dataSource);
+  }
+}

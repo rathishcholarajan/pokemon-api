7293272db184d13fe90a1f7b53370eaf7645d0f1
Rathish Cholarajan <rathishc24@gmail.com>
2020-09-15 21:39:31 -0400

    feat(seed): create seed pokemon json data in mongodb on app start and also provide disableSeeding option to disable overwriting

--- src/application.ts
+++ src/application.ts
@@ -1,13 +1,15 @@
 import {BootMixin} from '@loopback/boot';
 import {ApplicationConfig} from '@loopback/core';
+import {RepositoryMixin, SchemaMigrationOptions} from '@loopback/repository';
+import {RestApplication} from '@loopback/rest';
 import {
   RestExplorerBindings,
-  RestExplorerComponent,
+  RestExplorerComponent
 } from '@loopback/rest-explorer';
-import {RepositoryMixin} from '@loopback/repository';
-import {RestApplication} from '@loopback/rest';
 import {ServiceMixin} from '@loopback/service-proxy';
+import fs from 'fs';
 import path from 'path';
+import {PokemonRepository} from './repositories';
 import {MySequence} from './sequence';
 
 export {ApplicationConfig};
@@ -40,5 +42,33 @@ export class PokemonApplication extends BootMixin(
         nested: true,
       },
     };
+  }
+
+  // Unfortunately, TypeScript does not allow overriding methods inherited
+  // from mapped types. https://github.com/microsoft/TypeScript/issues/38496
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  async start(): Promise<void> {
+    // Use `databaseSeeding` flag to control if pokemon should be pre-
+    // populated into the database. Default is `true`.
+    if (this.options.databaseSeeding !== false) {
+      await this.migrateSchema();
+    }
+    return super.start();
+  }
+
+  async migrateSchema(options?: SchemaMigrationOptions) {
+    await super.migrateSchema(options);
+
+    // Pre-populate pokemon
+    const pokemonRepo = await this.getRepository(PokemonRepository);
+    await pokemonRepo.deleteAll();
+    const pokemonJsonFile = path.join(__dirname, '../data/pokemon.json');
+    const pokemonJsonString = fs.readFileSync(pokemonJsonFile, 'utf8');
+    const pokemonArray = JSON.parse(pokemonJsonString);
+
+    for (const pokemon of pokemonArray) {
+      await pokemonRepo.create(pokemon);
+    }
   }
 }

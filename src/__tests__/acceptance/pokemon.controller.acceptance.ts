20a56dd37e2b8dc95865709afd5bc0720a2580d2
Rathish Cholarajan <rathishc24@gmail.com>
2020-09-17 14:18:33 -0400

    test(pokemon): added more tests for pokemon controller routes to get pokemon by id or name and also added more red acceptance tests

--- src/__tests__/acceptance/pokemon.controller.acceptance.ts
+++ src/__tests__/acceptance/pokemon.controller.acceptance.ts
@@ -73,4 +73,39 @@ describe('PokemonController', () => {
     expect(res.body[0].name).to.equal('Ivysaur');
     expect(res.body[1].name).to.equal('Charmander');
   });
+
+  it('invokes GET /pokemon with invalid value', async () => {
+    const filter = {
+      where: {
+        id: "1000"
+      }
+    };
+    const res = await client.get(`/pokemon?filter=${JSON.stringify(filter)}`)
+      .expect(200);
+    expect(res.body.length).to.equal(0);
+  });
+
+  it('invokes GET /pokemon/{id} with pokemon id', async () => {
+    const res = await client.get(`/pokemon/1`)
+      .expect(200);
+    expect(res.body.name).to.equal('Bulbasaur');
+  });
+
+  it('invokes GET /pokemon/{name} with case insensitive pokemon name', async () => {
+    const res = await client.get(`/pokemon/iVySaur`)
+      .expect(200);
+    expect(res.body.name).to.equal('Ivysaur');
+  });
+
+  it('invokes GET /pokemon/{id} with invalid id', async () => {
+    const res = await client.get(`/pokemon/1000`)
+      .expect(404);
+    expect(res.body.error.message).to.equal('Pokemon not found for id: 1000.');
+  });
+
+  it('invokes GET /pokemon/{name} with invalid name', async () => {
+    const res = await client.get(`/pokemon/apple`)
+      .expect(404);
+    expect(res.body.error.message).to.equal('Pokemon not found for name: apple.');
+  });
 });

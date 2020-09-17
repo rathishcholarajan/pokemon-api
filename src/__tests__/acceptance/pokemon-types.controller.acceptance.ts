f122f27ba645dfa07b7c17f580c268a4a3f1d3ea
Rathish Cholarajan <rathishc24@gmail.com>
2020-09-17 14:02:21 -0400

    test(pokemon-types): created acceptance test for pokemon types controller

---
+++ src/__tests__/acceptance/pokemon-types.controller.acceptance.ts
@@ -0,0 +1,31 @@
+import {Client, expect} from '@loopback/testlab';
+import {PokemonApplication} from '../..';
+import {clearDatabase, setupDatabase} from '../helpers/database.helpers';
+import {setupApplication} from './test-helper';
+
+describe('PokemonTypesController', () => {
+  let app: PokemonApplication;
+  let client: Client;
+
+  before('setupApplication', async () => {
+    ({app, client} = await setupApplication());
+  });
+
+  before('setupDatabase', async () => {
+    clearDatabase();
+    setupDatabase();
+  });
+
+  after(async () => {
+    await app.stop();
+  });
+
+  it('invokes GET /pokemon-types', async () => {
+    const res = await client.get(`/pokemon-types`)
+      .expect(200);
+    expect(res.body).to.be.length(3);
+    expect(res.body[0].name).to.equal('Grass');
+    expect(res.body[1].name).to.equal('Poison');
+    expect(res.body[2].name).to.equal('Fire');
+  });
+});
